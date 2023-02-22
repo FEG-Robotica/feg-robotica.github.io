ac6de35cbda77b3034b4faa711e63262e19b4267
Flavio Monteiro <flaviomonteiro2013@gmail.com>
2023-02-22 04:18:14 -0300

    Adicionar gulp-imagemin e fazer o gulp-replace dos slides

---
+++ README.md
@@ -0,0 +1,27 @@
+# Site FEG Robótica
+Aqui você encontra informações de como editar o site
+
+## Setup
+1. Se já não o tiver, instale o [node.js](https://nodejs.org) - LTS ou Current,
+   tanto faz
+2. Abra seu terminal **na pasta desse repositorio** e digite `npm install`
+
+## Edição dos textos e mudança/adição de imagens
+1. Edite o `gulpfile.js`
+2. Abra seu terminal **na pasta desse repositorio** e digite `npm run build`
+3. Faça a commit
+
+## Lista de tarefas
+- [x] Adaptar seção de slideshow com o gulp-replace
+- [ ] Adaptar seção de sobre com o gulp-replace
+- [ ] Adaptar seção de robôs com o gulp-replace
+- [ ] Adaptar seção de projetos com o gulp-replace
+- [ ] Adaptar seção de patrocínios com o gulp-replace
+- [ ] Adaptar seção de membros com o gulp-replace
+- [ ] pegar o favicon
+- [x] Adicionar o gulp-imagemin
+- [ ] Adicionar captions aos slides
+
+## Dúvidas
+Quaisquer dúvidas contate-me em (12) 98219-6666 no Whatsapp ou
+flaviomonteiro2013@gmail.com
---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+import gulp from 'gulp'
+import imagemin from 'gulp-imagemin'
+import replace from 'gulp-replace'
+
+let makeSlideObj = (file, shortDescription) => {
+    return {
+        file: file,
+        shortDescription: shortDescription,
+    }
+}
+let slides = []
+slides.push(makeSlideObj('img/sample_16x9.png', 'Foto da equipe trabalhando'))
+slides.push(makeSlideObj('img/sample_16x9.png', 'Foto do projeto social'))
+slides.push(makeSlideObj('img/sample_16x9.png', 'Foto numa competição'))
+
+function replaceSlides() {
+
+
+    if (slides.length == 0) {
+        console.log('nenhum slide detectado, seção de slides excluída dessa build')
+        return ''
+    }
+
+
+
+    let retval = `<div class="carousel slide" data-bs-ride="carousel" id="carousel">
+                <div class="carousel-inner">`
+
+    slides.forEach(function (slideObj, indice) {
+        retval +=
+            `
+                    <div class="carousel-item@@IS_ACTIVE">
+                        <img src="@@IMAGE_SRC" class="d-block w-50 mx-auto" alt="@@IMAGE_DESCR">
+                    </div>`
+                .replace('@@IS_ACTIVE', (indice == 0 ? ' active' : ''))
+                .replace('@@IMAGE_SRC', slideObj.file)
+                .replace('@@IMAGE_DESCR', slideObj.shortDescription)
+    })
+
+    retval += `             
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </button>
+                    <div class="carousel-indicators">`
+
+    slides.forEach(function (slideObj, indice) {
+        retval +=
+            `
+                        <button data-bs-target="#carousel" type="button" data-bs-slide-to="@@INDICE"@@IS_ACTIVE></button>`
+                .replace('@@INDICE', indice)
+                .replace('@@IS_ACTIVE', (indice == 0 ? ' class="active"' : ''))
+    })
+
+    retval += `
+                    </div>
+                </div>
+    `
+    return retval
+}
+
+let comprimeImagens = function () {
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./img/'))
+}
+
+let substitui = function () {
+    let retval = gulp.src('./src/index.html')
+
+    // Substituir seção sobre
+    retval = retval.pipe(replace('@@SLIDES', replaceSlides()))
+
+    retval = retval.pipe(gulp.dest('./'))
+
+    return retval
+}
+
+export {
+    comprimeImagens as img,
+    substitui as replace
+}
+
+export default function () {
+    comprimeImagens()
+    substitui()
+}
